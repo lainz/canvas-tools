6de911bfa12d0e75ab51f74d28c119ce86ad3622
lainz <leandrodiazoe@gmail.com>
2020-07-19 20:14:50 -0300

    scanline

--- canvastools.js
+++ canvastools.js
@@ -14,10 +14,11 @@ function canvastools(id) {
 
     interface.iterate2d = function (cb) {
         var imageData = context.getImageData(0, 0, canvas.width, canvas.height)
-        for (var x = 0; x < canvas.width; x++) {
-            for (var y = 0; y < canvas.height; y++) {
-                var i = y * (canvas.width * 4) + x * 4
+        for (var y = 0; y < canvas.height; y++) {
+            var i = y * (canvas.width * 4)
+            for (var x = 0; x < canvas.width; x++) {
                 cb(imageData.data, i, i + 1, i + 2, i + 3, x, y)
+                i += 4
             }
         }
         context.putImageData(imageData, 0, 0)
