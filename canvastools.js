b00c48b49b969575f5686ec173ae1de20c56c3ca
lainz <leandrodiazoe@gmail.com>
2020-07-19 19:47:11 -0300

    iterate2d

--- canvastools.js
+++ canvastools.js
@@ -12,6 +12,18 @@ function canvastools(id) {
         return interface
     }
 
+    interface.iterate2d = function (cb) {
+        var imageData = context.getImageData(0, 0, canvas.width, canvas.height)
+        for (var x = 0; x < canvas.width; x++) {
+            for (var y = 0; y < canvas.height; y++) {
+                var i = y * (canvas.width * 4) + x * 4
+                cb(imageData.data, i, i + 1, i + 2, i + 3, x, y)
+            }
+        }
+        context.putImageData(imageData, 0, 0)
+        return interface
+    }
+
     interface.animate = function (cb) {
         window.requestAnimationFrame(cb)
         return interface
--- demo.js
+++ demo.js
@@ -2,12 +2,22 @@ var canvas = canvastools('canvas')
 
 function draw() {
     canvas
-        .iterate((data, red, green, blue, alpha) => {
+        /*.iterate((pixels, red, green, blue, alpha) => {
             var color = Math.random() > 0.5 ? 255 : 0
-            data[red] = color
-            data[green] = color
-            data[blue] = color
-            data[alpha] = 255
+            pixels[red] = color
+            pixels[green] = color
+            pixels[blue] = color
+            pixels[alpha] = 255
+        })*/
+        .iterate2d((pixels, red, green, blue, alpha, x, y) => {
+            var color = 0
+            if (y % 2 == 0) {
+                color = Math.random() > 0.5 ? 255 : 0
+            }
+            pixels[red] = color
+            pixels[green] = color
+            pixels[blue] = color
+            pixels[alpha] = 255
         })
         .animate(draw)
 }
